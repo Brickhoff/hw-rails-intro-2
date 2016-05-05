ce9ed352d20a9bccf6877c7130d4157f0eb2d01c
Khanh Tran <khanhtrannnn@gmail.com>
2016-05-05 19:25:27 +0000

    Done part 1: Ordering the name and release date of movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,25 +1,30 @@
+# This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
 
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  def index
+  case params[:sort]
+    when 'title'
+      @movies = Movie.order('title ASC')
+      @title_hilite = 'hilite'
+    when 'release'
+      @movies = Movie.order('release_date ASC')
+      @release_hilite = 'hilite'
+    else
+      @movies = Movie.all
+    end
   end
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
-  end
-
-  def index
-    @movies = Movie.all
+    id = params[:id] 
+    @movie = Movie.find(id) 
   end
 
   def new
-    # default: render 'new' template
   end
 
   def create
-    @movie = Movie.create!(movie_params)
+    debugger
+    @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -30,7 +35,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
+    @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
